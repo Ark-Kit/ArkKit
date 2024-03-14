5b8d765e8a0d79e994d5187dfd696b29032af73f
Didymus <didymusne@gmail.com>
2024-03-14 09:48:41 +0800

    [ark-ui-adaptor] feat: add template for viewRendererDelegate and viewHandlerDelegate

---
+++ ArkKit/ark-ui-adaptor/view-renderer-delegates/RendererDelegate.swift
@@ -0,0 +1,33 @@
+protocol RendererDelegate: AnyObject {
+    /**
+     * ```
+     * render(renderedGameState)
+     * ```
+     * ArkUiAdaptor implements the `render` function where it reads the `gameState` and renders
+     * assigned objects based on the `renderedGameState` with a default `GameStateRendererVisitor`.
+     *
+     * Devs can customise the rendering with their own `GameStateRendererVisitor`.
+     */
+    func render(from renderableGameState: RenderableGameState)
+}
+
+/**
+ * The Visitor will implement the rendering logic
+ */
+protocol GameStateRendererVisitor {
+    func visit()
+    /**
+     * An example implementation for GameRenderer:
+     * ```
+     *  func visit(_ gameObject: CircularGameObject) {
+     *      let circle = CircleUi(radius: gameObject.radius,
+     *                            center: CGPoint(gameObject.center))
+     *      circle.render(into: superview)
+     *  }
+     * ```
+     */
+}
+
+protocol RenderableGameState {
+    func accept(visitor: GameStateRendererVisitor)
+}
