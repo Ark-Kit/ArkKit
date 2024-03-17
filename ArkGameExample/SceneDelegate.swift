794c9826ec7876ede07c991d9d380e89c643ca85
Didymus <didymusne@gmail.com>
2024-03-17 17:46:58 +0800

    [ark-game-example] feat: add setup template for game example

--- ArkGameExample/SceneDelegate.swift
+++ ArkGameExample/SceneDelegate.swift
@@ -11,11 +11,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else {
+            return
+        }
+        window = UIWindow(windowScene: windowScene)
+        guard let window = window else {
+            return
+        }
+
+        window.rootViewController = RootViewController()
+        window.makeKeyAndVisible()
+        let arkBlueprint = defineArkBlueprint()
+        loadArkBlueprintToScene(arkBlueprint, window: window)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -46,4 +56,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 
+}
+
+extension SceneDelegate {
+    func defineArkBlueprint() -> ArkBlueprint {
+        // Define game with blueprint here.
+        let arkBlueprint = ArkBlueprint()
+        return arkBlueprint
+    }
+    func loadArkBlueprintToScene(_ blueprint: ArkBlueprint, window: UIWindow) {
+        guard let rootView = window.rootViewController as? UINavigationController else {
+            return
+        }
+        let arkInstance = Ark(rootView: rootView)
+        arkInstance.start(blueprint: blueprint)
+    }
 }
