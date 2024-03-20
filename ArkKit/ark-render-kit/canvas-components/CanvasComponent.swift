407422b94cd917b98297af94ec5f3c94db45fbf8
Didymus <didymusne@gmail.com>
2024-03-20 20:03:47 +0800

    [ark-render] refactor: update memoizable canvas method signature

--- ArkKit/ark-render-kit/canvas-components/CanvasComponent.swift
+++ ArkKit/ark-render-kit/canvas-components/CanvasComponent.swift
@@ -5,11 +5,11 @@ protocol CanvasComponent: Component, Memoizable {
 protocol Memoizable {
     typealias AreValuesEqualDelegate = (_: Self, _: Self) -> Bool
     var areValuesEqual: AreValuesEqualDelegate { get }
-    func hasUpdated(previous: any CanvasComponent) -> Bool
+    func hasUpdated(previous: any Memoizable) -> Bool
 }
 
 extension CanvasComponent {
-    func hasUpdated(previous: any CanvasComponent) -> Bool {
+    func hasUpdated(previous: any Memoizable) -> Bool {
         guard let previousComp = previous as? Self else {
             return true
         }
