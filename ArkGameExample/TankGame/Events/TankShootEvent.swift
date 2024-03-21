ff4abd566da3411f2cdf9a5b07d0b0c4c59b0bde
ryanpeh <qwertz0808@gmail.com>
2024-03-22 02:25:30 +0800

    [tank-demo-game] feat: add tank firing

---
+++ ArkGameExample/TankGame/Events/TankShootEvent.swift
@@ -0,0 +1,26 @@
+//
+//  TankShootEvent.swift
+//  ArkKit
+//
+//  Created by Ryan Peh on 21/3/24.
+//
+
+import Foundation
+
+struct TankShootEventData: ArkEventData {
+    var name: String
+    var tankEntity: Entity
+}
+
+struct TankShootEvent: ArkEvent {
+
+    static var id = UUID()
+    var eventData: ArkEventData?
+    var timestamp = Date()
+    var priority: Int?
+
+    init(eventData: ArkEventData? = nil, priority: Int? = nil) {
+        self.eventData = eventData
+        self.priority = priority
+    }
+}
