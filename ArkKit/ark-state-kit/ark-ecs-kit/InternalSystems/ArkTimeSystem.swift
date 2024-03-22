35ea6ac8b81bcfe2c8b660ec462f99e5bf8d73e3
Markus Yeo <markusyeojy@hotmail.com>
2024-03-22 13:36:38 +0800

    [ark-ecs-kit] add Internal ArkTimeSystem and StopWatchComponent

---
+++ ArkKit/ark-state-kit/ark-ecs-kit/InternalSystems/ArkTimeSystem.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+/**
+ *  A system which updates a StopWatchComponent instances in ArkECS after the given delta.
+ */
+class ArkTimeSystem: System {
+    var active: Bool
+    
+    init(active: Bool = true) {
+        self.active = active
+    }
+    
+    func update(deltaTime: TimeInterval, arkECS: ArkECS) {
+        let stopWatchEntities = arkECS.getEntities(with: [StopWatchComponent.self])
+        for stopWatchEntity in stopWatchEntities {
+            guard var stopWatchComponent = arkECS.getComponent(ofType: StopWatchComponent.self, for: stopWatchEntity) else {
+                return
+            }
+            stopWatchComponent.currentTime += deltaTime
+            arkECS.upsertComponent(stopWatchComponent, to: stopWatchEntity)
+        }
+    }
+}
