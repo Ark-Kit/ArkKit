a4bbe2f2b361c31a4b39b5aadf8e7c8f0c23ff1b
Didymus <didymusne@gmail.com>
2024-03-23 19:30:34 +0800

    [misc] chore: remove commented code (cleanup)

--- ArkKit/ark-render-kit/ArkCanvas.swift
+++ ArkKit/ark-render-kit/ArkCanvas.swift
@@ -1,6 +1,6 @@
 struct ArkCanvas: Canvas {
-    var componentsToUnmount: [EntityID: [RenderableComponentType: (any Renderable)?]] = [:]
-    var componentsToMount: [EntityID: [RenderableComponentType: any RenderableComponent]] = [:]
+    private(set) var componentsToUnmount: [EntityID: [RenderableComponentType: (any Renderable)?]] = [:]
+    private(set) var componentsToMount: [EntityID: [RenderableComponentType: any RenderableComponent]] = [:]
 
     mutating func addComponentToUnmount(entityId: EntityID,
                                         componentType: RenderableComponentType,
@@ -20,32 +20,4 @@ struct ArkCanvas: Canvas {
             componentsToMount[entityId] = [componentType: renderableComponent]
         }
     }
-//    var canvasComponentsToRender: [(Entity, any RenderableComponent, any RenderableComponent.Type)] = []
-//    var canvasRenderablesToUnmount: [(Entity, any RenderableComponent.Type)] = []
-//
-//    func render(using renderer: any CanvasRenderer, to context: CanvasContext) {
-//        for (entity, canvasComponent, compType) in canvasComponentsToRender {
-//            if let (_, prevRenderable) = context.memo[entity]?[ObjectIdentifier(compType)] {
-//                prevRenderable.unmount()
-//            }
-//            let renderable = canvasComponent.render(using: renderer)
-//            context.saveToMemo(entity: entity, canvasComponentType: compType,
-//                               canvasComponent: canvasComponent, renderable: renderable)
-//        }
-//    }
-//
-//    func unmount(from context: CanvasContext) {
-//        for (entity, canvasComponentType) in canvasRenderablesToUnmount {
-//            let renderable = context.removeFromMemo(entity: entity, canvasComponentType: canvasComponentType)
-//            renderable?.unmount()
-//        }
-//    }
-//    mutating func addToRender(entity: Entity,
-//                              canvasComponent: any RenderableComponent,
-//                              compType: any RenderableComponent.Type) {
-//        canvasComponentsToRender.append((entity, canvasComponent, compType))
-//    }
-//    mutating func addToUnmount(entity: Entity, compType: any RenderableComponent.Type, context: CanvasContext) {
-//        canvasRenderablesToUnmount.append((entity, compType))
-//    }
 }
