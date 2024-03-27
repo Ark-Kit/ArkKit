13512358f59f53033437242040f83d5584fe4af4
Didymus <didymusne@gmail.com>
2024-03-27 14:22:37 +0800

    [ark-loop] refactor: expand loop to have optional physics world update delegate and game world update delegate

--- ArkKit/ark-loop-kit/GameLoop.swift
+++ ArkKit/ark-loop-kit/GameLoop.swift
@@ -1,5 +1,16 @@
 protocol GameLoop {
+    var updatePhysicsSceneDelegate: ArkPhysicsSceneUpdateLoopDelegate? { get set }
+    var updateGameWorldDelegate: ArkGameWorldUpdateLoopDelegate? { get set }
     func setUp()
+    func update()
     func getDeltaTime() -> Double
     func shutDown()
+}
+
+protocol ArkGameWorldUpdateLoopDelegate: AnyObject {
+    func update(for dt: Double)
+}
+
+protocol ArkPhysicsSceneUpdateLoopDelegate: AnyObject {
+    func update(_ deltaTime: Double)
 }
