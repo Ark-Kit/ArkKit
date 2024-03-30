0418bac253bf5c472788f331316f175c9d7561d4
Didymus <didymusne@gmail.com>
2024-03-30 14:52:48 +0800

    [ark-render, ark-uikit] feat: add button styling config

--- ArkKit/ark-game/view/ui-kit/view-components/input/UIKitButton.swift
+++ ArkKit/ark-game/view/ui-kit/view-components/input/UIKitButton.swift
@@ -7,8 +7,6 @@ final class UIKitButton: UIButton, UIKitRenderable, TapRenderable {
         let frame = CGRect(x: center.x - width / 2, y: center.y - height / 2,
                            width: width, height: height)
         super.init(frame: frame)
-        // TODO: enable styling of button from abstractButton
-        self.backgroundColor = .brown
         setUpTap()
     }
 
@@ -40,5 +38,44 @@ extension UIKitButton {
             .if(modifierInfo.onTapDelegate != nil, transform: { view in
                 view.onTap(modifierInfo.onTapDelegate ?? {})
             })
+    }
+}
+
+extension UIKitButton {
+    func style(_ styleConfig: ButtonStyleConfiguration,
+               colorGetter: (AbstractColor) -> UIColor) -> Self {
+        var configuration = self.configuration ?? UIButton.Configuration.filled()
+        // Set title for different states if provided
+        if let label = styleConfig.labelMapping {
+            self.setTitle(label.0, for: .normal)
+            self.setTitleColor(colorGetter(label.1), for: .normal)
+        }
+        configuration.baseBackgroundColor = colorGetter(
+            styleConfig.backgroundColor ?? .default
+        )
+        // Set padding if provided
+        if let padding = styleConfig.padding {
+            // Set content insets based on padding
+            configuration.contentInsets = NSDirectionalEdgeInsets(
+                top: padding[.top] ?? 0,
+                leading: padding[.left] ?? 0,
+                bottom: padding[.bottom] ?? 0,
+                trailing: padding[.right] ?? 0
+            )
+        }
+
+        if let borderRadius = styleConfig.borderRadius {
+            configuration.background.cornerRadius = borderRadius
+            configuration.cornerStyle = .fixed
+        }
+        if let borderWidth = styleConfig.borderWidth {
+            configuration.background.strokeWidth = borderWidth
+        }
+        if let borderColor = styleConfig.borderColor {
+            configuration.background.strokeColor = colorGetter(borderColor)
+        }
+
+        self.configuration = configuration
+        return self
     }
 }
