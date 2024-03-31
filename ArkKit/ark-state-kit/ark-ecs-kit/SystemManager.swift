41edad373d44e9bc3804cd5d9aeeb2d27a4ca715
Didymus <didymusne@gmail.com>
2024-03-31 17:34:27 +0800

    [ark-ecs] refactor: remove schedule

--- ArkKit/ark-state-kit/ark-ecs-kit/SystemManager.swift
+++ ArkKit/ark-state-kit/ark-ecs-kit/SystemManager.swift
@@ -7,49 +7,28 @@
 
 import Foundation
 
-enum Schedule {
-    case startup // only on first run
-    case update
-    case finish // finish systems to close when app is done
-}
-
 class SystemManager {
     typealias SystemType = ObjectIdentifier
 
-    private(set) var scheduleToSystemMapping: [Schedule: OrderedDictionary<SystemType, System>] = [
-        .startup: OrderedDictionary(),
-        .update: OrderedDictionary(),
-        .finish: OrderedDictionary()
-    ]
+    private(set) var systems: OrderedDictionary<SystemType, System> = OrderedDictionary()
 
-    func add(_ system: System, schedule: Schedule = .update) {
+    func add(_ system: System) {
         let identifier = ObjectIdentifier(type(of: system))
-        scheduleToSystemMapping[schedule]?[identifier] = system
+        systems[identifier] = system
     }
 
     func remove<T: System>(ofType type: T.Type) {
         let identifier = ObjectIdentifier(type)
-        for (schedule, _) in scheduleToSystemMapping {
-            scheduleToSystemMapping[schedule]?.removeValue(forKey: identifier)
-        }
+        systems.removeValue(forKey: identifier)
     }
 
     func system<T: System>(ofType type: T.Type) -> T? {
         let identifier = ObjectIdentifier(type)
-        for systemDict in scheduleToSystemMapping.values {
-            if let system = systemDict[identifier] {
-                return system as? T
-            }
-        }
-        return nil
+        return systems[identifier] as? T
     }
 
-    /// Runs the systems wihtin the `.update` schedule.
     func update(deltaTime: TimeInterval, arkECS: ArkECS) {
-        guard let updateSystems = scheduleToSystemMapping[.update] else {
-            return
-        }
-        for (_, system) in updateSystems {
+        for (_, system) in systems {
             system.update(deltaTime: deltaTime, arkECS: arkECS)
         }
     }
