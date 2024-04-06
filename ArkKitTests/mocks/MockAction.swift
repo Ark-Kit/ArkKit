d58791daa38426b6440fbee51bb9ad569afebce1
En Rong <53928333+chownces@users.noreply.github.com>
2024-04-07 00:44:09 +0800

    [test] fix: mocks

--- ArkKitTests/mocks/MockAction.swift
+++ ArkKitTests/mocks/MockAction.swift
@@ -1,13 +1,13 @@
 @testable import ArkKit
 
-class MockAction<Event: ArkEvent>: Action {
+class MockAction<Event: ArkEvent, AudioEnum: ArkAudioEnum>: Action {
     var priority: Int = 0
 
     // Implement the execute method to store executed events and contexts
-    private(set) var executedEvents: [(event: Event, context: ArkActionContext)] = []
+    private(set) var executedEvents: [(event: Event, context: ArkActionContext<AudioEnum>)] = []
 
     // Implement the execute method required by the Action protocol
-    func execute(_ event: Event, context: ArkActionContext) {
+    func execute(_ event: Event, context: ArkActionContext<AudioEnum>) {
         // Store the executed event and context
         executedEvents.append((event, context))
     }
